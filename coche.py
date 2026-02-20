1dc4e2dea37405dffaad97b2392ee4627d48549e
pca <madfut1010@gmail.com>
2026-02-20 09:57:24 +0100

    Actualizacion del fichero coche

--- coche.py
+++ coche.py
@@ -1,1 +1,13 @@
-print('Hola')
+class Coche:
+    kilometros_recorridos = 0
+    gasolina = 0
+    def __init__(self, matricula, marca):
+        self.matricula = matricula
+        self.marca = marca
+
+class Persona:
+    def __init__(self, dni, nombre, apellido, coche):
+        self.dni = dni
+        self.nombre = nombre
+        self.apellido = apellido
+        self.coche = coche
